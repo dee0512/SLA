9aabad3b28709eed94542db5c10e855c677907d7
Devdhar Patel <devdharpatel@gmail.com>
2024-07-15 20:13:16 -0700

    Create bulk_train_TD3.sh

---
+++ bulk_train_TD3.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+env_name=InvertedPendulum-v2
+count=0
+for seed in {0..9}; do
+        if ! [ -f "models/TD3_${env_name}_${seed}_final_actor" ];
+        then
+          sbatch train_TD3.sh $seed $env_name
+          count=$((count + 1))
+        fi
+done
+echo Launched $count jobs
